9169f4f3b7f5aa3eb03719496e08e2d2f7142a57
Mohamed Abdulhakim <69153321+MohamedCK@users.noreply.github.com>
2022-09-14 12:30:38 +0200

    Add project structure with react components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
+import Calculator from './Components/Calculator';
 import './index.css';
-// eslint-disable-next-line import/extensions
-import Calculator from './Components/Calculator ';
 
 class App extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
   render() {
     return (<Calculator />);
   }
