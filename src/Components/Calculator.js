a52260606d036c0a66ec9216a945fe9d99a79daa
Mohamed Abdulhakim <69153321+MohamedCK@users.noreply.github.com>
2022-09-15 17:30:42 +0200

    Set events for the buttons in the calculator

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,43 +1,69 @@
 import React from 'react';
 import '../index.css';
+import Calculate from '../logic/Calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+  }
+
+   handleClick = (events) => {
+     const { value } = events.target;
+     const answer = Calculate(this.state, value);
+     this.setState(answer);
+   }
+
+  showAnswer = (events) => {
+    this.setState({
+      total: events.target.value,
+    });
+  }
+
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="calcCenter">
         <div className="mainCalculator">
-          <div className="screen-row">
-            <input type="text" value="88" />
+          <div className="screen-row" onChange={this.showAnswer}>
+            {total}
+            {next}
+            {operation}
           </div>
           <div className="button-row one">
-            <input type="button" value="AC" />
-            <input type="button" value="+/-" />
-            <input type="button" value="%" />
-            <input type="button" value="/" className="orange" />
+            <input type="button" value="AC" onClick={this.handleClick} />
+            <input type="button" value="+/-" onClick={this.handleClick} />
+            <input type="button" value="%" onClick={this.handleClick} />
+            <input type="button" value="/" className="orange" onClick={this.handleClick} />
           </div>
           <div className="button-row two">
-            <input type="button" value="7" />
-            <input type="button" value="8" />
-            <input type="button" value="9" />
-            <input type="button" value="*" className="orange" />
+            <input type="button" value="7" onClick={this.handleClick} />
+            <input type="button" value="8" onClick={this.handleClick} />
+            5
+            <input type="button" value="9" onClick={this.handleClick} />
+            <input type="button" value="*" onClick={this.handleClick} className="orange" />
           </div>
           <div className="button-row three">
-            <input type="button" value="4" />
-            <input type="button" value="5" />
-            <input type="button" value="6" />
-            <input type="button" value="-" className="orange" />
+            <input type="button" value="4" onClick={this.handleClick} />
+            <input type="button" value="5" onClick={this.handleClick} />
+            <input type="button" value="6" onClick={this.handleClick} />
+            <input type="button" value="-" onClick={this.handleClick} className="orange" />
           </div>
           <div className="button-row four">
-            <input type="button" value="1" />
-            <input type="button" value="2" />
-            <input type="button" value="3" />
-            <input type="button" value="+" className="orange" />
+            <input type="button" value="1" onClick={this.handleClick} />
+            <input type="button" value="2" onClick={this.handleClick} />
+            <input type="button" value="3" onClick={this.handleClick} />
+            <input type="button" value="+" onClick={this.handleClick} className="orange" />
           </div>
           <div className="button-row five">
-            <input type="button" value="0" className="zero" />
-            <input type="button" value="." />
-            <input type="button" value="=" className="orange" />
+            <input type="button" value="0" onClick={this.handleClick} className="zero" />
+            <input type="button" value="." onClick={this.handleClick} />
+            <input type="button" value="=" onClick={this.handleClick} className="orange" />
           </div>
         </div>
       </div>
