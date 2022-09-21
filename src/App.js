19e62ad64a3e9ea3a05d259dfccc58181286e745
Mohamed Abdulhakim <69153321+MohamedCK@users.noreply.github.com>
2022-09-21 20:18:38 +0200

    Use react route nest all pages and style it uss css modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './pages/Navbar';
+import Home from './pages/Home';
+import Quote from './pages/Quotes';
 import Calculator from './Components/Calculator';
 import './index.css';
 
 class App extends React.PureComponent {
   render() {
-    return (<Calculator />);
+    return (
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="about" element={<Quote />} />
+        </Routes>
+      </>
+    );
   }
 }
 
