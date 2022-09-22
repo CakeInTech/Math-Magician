2447e7b7b7a4e14d4fa15e28f2c7673265666556
Mohamed Abdulhakim <69153321+MohamedCK@users.noreply.github.com>
2022-09-22 17:13:53 +0200

    Implement a Test for the navbar

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import css from './Navbar.module.css';
 
 const links = [
@@ -7,30 +6,33 @@ const links = [
     id: 1,
     path: '/home', 
     text: 'home',
+    data_testid: 'home',
   }, 
   {
     id: 2,
     path: '/calculator', 
     text: 'Calculator',
+    data_testid: 'calculator',
   },
   {
       id: 3,
       path: 'about',
-      text: 'About'
+      text: 'About',
+      data_testid: 'about',
   },
 
 ];
 
 const Navbar = () => (
-   <div className={css.Navigation}>
+   <div className={css.Navigation} data_testid='navbar'>
     <h1>Math Magician</h1>
     <ul className={css.items}>
         {links.map((links) => (
             <li key={links.id}>
                 {""}
-                <NavLink to={links.path} className={css.Nav_link}>
+              <a to={links.path} className={css.Nav_link} data_testid={links.data_testid}>
                   {links.text}
-                </NavLink>
+              </a>
             </li>
         ))}
     </ul>
---
+++ src/pages/Navbar.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, cleanup, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Home from './Home';
+import Quote from './Quotes';
+
+
+afterEach(() => {
+  cleanup();
+})
+
+describe('User interaction test', () => {
+  it('Checks when clicked if the home component works', () => {
+    render(<Home />);
+    const element = screen.getByTestId('home');
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveTextContent('Welcome to our page!Lorem ipsum dolor sit amet consectetur adipisicing elit. In eaque illo et asperiores temporibus tempore ad aperiam at fuga distinctio labore quasi maiores laudantium odio consectetur eveniet earum consequatur, dignissimos, deleniti eos minus.',)
+  });
+});
+
+it('Checks when clicked if the Quote component works', () => {
+  render(<Quote />);
+  const element = screen.getByTestId('quote');
+  expect(element).toBeInTheDocument();
+  expect(element).toHaveTextContent(
+    'Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. -William Paul Thurston',
+  );
+});
